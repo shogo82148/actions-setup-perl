3897eaaffb00bd384b6a976919170b2a9115b224
Ichinose Shogo <shogo82148@gmail.com>
2019-09-16 18:23:11 +0900

    download perl source code

--- build-perl-win.pl
+++ build-perl-win.pl
@@ -1,10 +1,18 @@
 #!C:\Strawberry\perl\bin\perl.exe
 
 use utf8;
-use warning;
+use warnings;
 use strict;
 use 5.026002;
+use LWP::UserAgent;
 
-print "hello perl!";
+my $ua = LWP::UserAgent->new;
+my $response = $ua->get("https://cpan.metacpan.org/authors/id/X/XS/XSAWYERX/perl-5.30.0.tar.gz"); # TODO: auto generate url
+if (!$response->is_success) {
+    die "download failed: " . $response->status_line;
+}
+
+open my $fh, ">", "perl-5.30.0.tar.gz" or die "$!"; 
+print $fh $response->content;
 
 1;

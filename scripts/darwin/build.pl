f54f6d530d12e979ae5975eef38cf0838e41845d
Ichinose Shogo <shogo82148@gmail.com>
2019-11-25 19:31:33 +0900

    fix PERL5LIB

---
+++ scripts/darwin/build.pl
@@ -0,0 +1,57 @@
+#!/bin/env bash
+
+use utf8;
+use warnings;
+use strict;
+use Try::Tiny;
+use Perl::Build;
+
+local $| = 1;
+
+sub group {
+    my ($name, $sub) = @_;
+    try {
+        print "::group::$name\n";
+        $sub->();
+    } catch {
+        die $_;
+    } finally {
+        print "::endgroup::\n";
+    };
+}
+
+sub run {
+    my $version = $ENV{PERL_VERSION};
+    my $install_dir = "$ENV{RUNNER_TOOL_CACHE}/perl/${version}/x64";
+    my $tmpdir = $ENV{RUNNER_TEMP};
+
+    group "build perl $version" => sub {
+        Perl::Build->install_from_cpan(
+            $version => (
+                dst_path          => $install_dir,
+                configure_options => ["-Dman1dir=none", "-Dman3dir=none"],
+                jobs              => 2,
+            )
+        );
+    };
+
+    group "install App::cpanminus and Carton" => sub {
+        system("$install_dir/bin/cpan", "-T", "App::cpanminus", "Carton") == 0
+            or die "Failed to install App::cpanminus and Carton";
+    };
+
+    group "archiving" => sub {
+        chdir $install_dir or die "failed to cd $install_dir: $!";
+        system("tar", "zcf", "$tmpdir/perl.tar.gz", ".") == 0
+            or die "failed to archive";
+    };
+}
+
+try {
+    run();
+} catch {
+    print "::error::$_\n";
+    exit 1;
+};
+
+1;

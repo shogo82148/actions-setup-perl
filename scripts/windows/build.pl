aaa3156a26e2d43fd93ae7a022c8a48c265565d3
Ichinose Shogo <shogo82148@gmail.com>
2019-11-27 21:28:43 +0900

    fix command name

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -91,8 +91,7 @@ sub run {
         print $fh $response->content;
         close $fh;
 
-        local $ENV{PERL_DL_DEBUG} = "10";
-        system(File::Spec->catfile($install_dir, "bin", "perl") . "-Dtls $cpanm --notest App::cpanminus Carton") == 0
+        system(File::Spec->catfile($install_dir, "bin", "perl") . " -Dtls $cpanm --notest App::cpanminus Carton") == 0
             or die "Failed to install App::cpanminus and Carton";
     };
 

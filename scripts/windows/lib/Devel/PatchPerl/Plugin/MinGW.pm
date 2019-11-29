c0e5984c4f1815d2314555f6df5babb19670c26a
Ichinose Shogo <shogo82148@gmail.com>
2019-11-30 01:08:25 +0900

    add debug message

--- scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
+++ scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
@@ -1542,6 +1542,38 @@ sub _patch_make_maker_dirfilesep {
 -=cut
 -
 -
+--- ext/DynaLoader/dl_win32.xs
++++ ext/DynaLoader/dl_win32.xs
+@@ -126,13 +126,13 @@ dl_load_file(filename,flags=0)
+     CODE:
+   {
+     PERL_UNUSED_VAR(flags);
+-    DLDEBUG(1,PerlIO_printf(Perl_debug_log,"dl_load_file(%s):\n", filename));
++    PerlIO_printf(Perl_debug_log,"dl_load_file(%s):\n", filename);
+     if (dl_static_linked(filename) == 0) {
+ 	retv = PerlProc_DynaLoad(filename);
+     }
+     else
+ 	retv = (void*) Win_GetModuleHandle(NULL);
+-    DLDEBUG(2,PerlIO_printf(Perl_debug_log," libref=%x\n", retv));
++    PerlIO_printf(Perl_debug_log," libref=%x\n", retv);
+ 
+     if (retv == NULL) {
+ 	SaveError(aTHX_ "load_file:%s",
+@@ -148,11 +148,11 @@ int
+ dl_unload_file(libref)
+     void *	libref
+   CODE:
+-    DLDEBUG(1,PerlIO_printf(Perl_debug_log, "dl_unload_file(%lx):\n", PTR2ul(libref)));
++    PerlIO_printf(Perl_debug_log, "dl_unload_file(%lx):\n", PTR2ul(libref));
+     RETVAL = FreeLibrary((HMODULE)libref);
+     if (!RETVAL)
+         SaveError(aTHX_ "unload_file:%s", OS_Error_String(aTHX)) ;
+-    DLDEBUG(2,PerlIO_printf(Perl_debug_log, " retval = %d\n", RETVAL));
++    PerlIO_printf(Perl_debug_log, " retval = %d\n", RETVAL);
+   OUTPUT:
+     RETVAL
+ 
 PATCH
 }
 

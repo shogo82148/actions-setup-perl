6d2aa9f191aed6f059956bbe89c2a0ca2cc8c7bc
Ichinose Shogo <shogo82148@gmail.com>
2019-12-04 00:34:25 +0900

    add scripts for fat-packing the App::cpanminus module

---
+++ author/cpanm/build.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use FindBin;
+use App::FatPacker::Simple;
+use Carton::Snapshot;
+
+sub fatpack {
+    App::FatPacker::Simple->new->parse_options(@_)->run
+}
+
+fatpack(
+    "-o", "cpanm",
+    "-d", "local",
+    "-e", join(
+        ',',
+        'App::cpanminus::fatscript',
+        # test modules
+        'Test2,App::Prove','TAP::Harness',
+        # core modules of perl 5
+        'Cwd'
+    ),
+    "--shebang", '#!/usr/bin/env perl',
+    "local/bin/cpanm"
+);

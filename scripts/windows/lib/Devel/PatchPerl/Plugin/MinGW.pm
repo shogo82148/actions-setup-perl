30ed3f9914663da5dd7cf1aac7e79c1489f7fcb8
Ichinose Shogo <shogo82148@gmail.com>
2019-12-10 19:01:48 +0900

    fix _patch_make_maker_508

--- scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
+++ scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
@@ -34,7 +34,6 @@ my @patch = (
     {
         perl => [
             qr/^5\.10\.0$/,
-            qr/^5\.[0-9]\./,
         ],
         subs => [
             [ \&_patch_make_maker_510 ],
@@ -202,6 +201,7 @@ my @patch = (
             qr/^5\.[89]\./,
         ],
         subs => [
+            [ \&_patch_make_maker_508 ],
             [ \&_patch_gnumakefile_508 ],
         ],
     },
@@ -10208,6 +10208,75 @@ sub _patch_threads {
  #endif
  #ifdef HAS_PPPORT_H
  #  define NEED_PL_signals
+PATCH
+}
+
+sub _patch_make_maker_508 {
+    # Win32 gmake needs SHELL to be specified
+    _patch(<<'PATCH');
+--- lib/ExtUtils/MM_Unix.pm
++++ lib/ExtUtils/MM_Unix.pm
+@@ -415,8 +415,8 @@ sub const_cccmd {
+ 
+ =item const_config (o)
+ 
+-Defines a couple of constants in the Makefile that are imported from
+-%Config.
++Sets SHELL if needed, then defines a couple of constants in the Makefile
++that are imported from %Config.
+ 
+ =cut
+ 
+@@ -427,6 +427,7 @@ sub const_config {
+     my(@m,$m);
+     push(@m,"\n# These definitions are from config.sh (via $INC{'Config.pm'})\n");
+     push(@m,"\n# They may have been overridden via Makefile.PL or on the command line\n");
++    push(@m, $self->specify_shell()); # Usually returns empty string
+     my(%once_only);
+     foreach $m (@{$self->{CONFIG}}){
+ 	# SITE*EXP macros are defined in &constants; avoid duplicates here
+@@ -3304,6 +3305,16 @@ $target :: $plfile
+     join "", @m;
+ }
+ 
++=item specify_shell
++
++Specify SHELL if needed - not done on Unix.
++
++=cut
++
++sub specify_shell {
++  return '';
++}
++
+ =item quote_paren
+ 
+ Backslashes parentheses C<()> in command line arguments.
+--- lib/ExtUtils/MM_Win32.pm
++++ lib/ExtUtils/MM_Win32.pm
+@@ -781,6 +781,22 @@ sub pasthru {
+     return "PASTHRU = " . ($NMAKE ? "-nologo" : "");
+ }
+ 
++=item specify_shell
++
++Set SHELL to $ENV{COMSPEC} only if make is type 'gmake'.
++
++=cut
++
++sub specify_shell {
++    my $self = shift;
++    return '' unless $self->is_make_type('gmake');
++    "\nSHELL = $ENV{COMSPEC}\n";
++}
++
++sub is_make_type {
++    my($self, $type) = @_;
++    return !! ($self->make =~ /\b$type(?:\.exe)?$/);
++}
+ 
+ 1;
+ __END__
 PATCH
 }
 

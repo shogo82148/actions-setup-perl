9dd9ea8d68e51d9ed23ccc2e6498b832b62eaf25
Ichinose Shogo <shogo82148@gmail.com>
2019-12-20 08:18:34 +0900

    fix config_H.gc

--- scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
+++ scripts/windows/lib/Devel/PatchPerl/Plugin/MinGW.pm
@@ -2042,11 +2042,11 @@ PATCH
         return;
     }
 
-    if (version->parse("v$version") >= version->parse("5.8.2")) {
+    if (version->parse("v$version") >= version->parse("5.8.3")) {
         _patch(<<'PATCH');
 --- win32/config_H.gc
 +++ win32/config_H.gc
-@@ -2363,14 +2363,18 @@
+@@ -1825,7 +1824,9 @@
   *	available to exclusively create and open a uniquely named
   *	temporary file.
   */
@@ -2055,8 +2055,9 @@ PATCH
 +#define HAS_MKSTEMP
 +#endif
  
- /* HAS_MKSTEMPS:
-  *	This symbol, if defined, indicates that the mkstemps routine is
+ /* HAS_MMAP:
+  *	This symbol, if defined, indicates that the mmap system call is
+@@ -2614,7 +2615,9 @@
   *	available to excluslvely create and open a uniquely named
   *	(with a suffix) temporary file.
   */
@@ -2065,8 +2066,8 @@ PATCH
 +#define HAS_MKSTEMPS
 +#endif
  
- /* HAS_MMAP:
-  *	This symbol, if defined, indicates that the mmap system call is
+ /* HAS_MODFL:
+  *	This symbol, if defined, indicates that the modfl routine is
 --- win32/win32.c
 +++ win32/win32.c
 @@ -1094,6 +1094,7 @@ chown(const char *path, uid_t owner, gid_t group)
@@ -2088,64 +2089,6 @@ PATCH
 --- win32/win32.h
 +++ win32/win32.h
 @@ -292,7 +292,9 @@ extern  void	*sbrk(ptrdiff_t need);
- #endif
- extern	char *	getlogin(void);
- extern	int	chown(const char *p, uid_t o, gid_t g);
-+#if !defined(__MINGW64_VERSION_MAJOR) || __MINGW64_VERSION_MAJOR < 4
- extern  int	mkstemp(const char *path);
-+#endif
- 
- #undef	 Stat
- #define  Stat		win32_stat
-PATCH
-        return;
-    }
-
-    if (version->parse("v$version") >= version->parse("5.8.1")) {
-        _patch(<<'PATCH');
---- win32/config_H.gc
-+++ win32/config_H.gc
-@@ -905,7 +905,7 @@
-  *	Quad_t, and its unsigned counterpar, Uquad_t. QUADKIND will be one
-  *	of QUAD_IS_INT, QUAD_IS_LONG, QUAD_IS_LONG_LONG, or QUAD_IS_INT64_T.
-  */
--/*#define HAS_QUAD	/**/
-+#define HAS_QUAD
- #ifdef HAS_QUAD
- #   define Quad_t long long	/**/
- #   define Uquad_t unsigned long long	/**/
-@@ -1819,7 +1819,9 @@
-  *	available to exclusively create and open a uniquely named
-  *	temporary file.
-  */
--/*#define HAS_MKSTEMP		/**/
-+#if __MINGW64_VERSION_MAJOR >= 4
-+#define HAS_MKSTEMP
-+#endif
- 
- /* HAS_MMAP:
-  *	This symbol, if defined, indicates that the mmap system call is
---- win32/win32.c
-+++ win32/win32.c
-@@ -986,6 +986,7 @@ chown(const char *path, uid_t owner, gid_t group)
-  * XXX this needs strengthening  (for PerlIO)
-  *   -- BKS, 11-11-200
- */
-+#if !defined(__MINGW64_VERSION_MAJOR) || __MINGW64_VERSION_MAJOR < 4
- int mkstemp(const char *path)
- {
-     dTHX;
-@@ -1006,6 +1007,7 @@ retry:
- 	goto retry;
-     return fd;
- }
-+#endif
- 
- static long
- find_pid(int pid)
---- win32/win32.h
-+++ win32/win32.h
-@@ -266,7 +266,9 @@ extern  void	*sbrk(ptrdiff_t need);
  #endif
  extern	char *	getlogin(void);
  extern	int	chown(const char *p, uid_t o, gid_t g);

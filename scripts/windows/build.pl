22af2fd508ae82ab30aa606b0ad5cd807154d6ef
Ichinose Shogo <shogo82148@gmail.com>
2020-11-06 16:00:50 +0900

    install OpenSSL

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -49,6 +49,43 @@ sub run {
     my $install_dir = File::Spec->rel2abs(
         File::Spec->catdir($ENV{RUNNER_TOOL_CACHE} || $tmpdir, "perl", $version, "x64"));
 
+    # NASM is required by OpenSSL
+    group "install nasm" => sub {
+        execute_or_die("choco install nasm");
+        $ENV{PATH} = 'C:\Program Files\NASM;' . $ENV{PATH};
+    };
+
+    group "downloading OpenSSL" => sub {
+        my $OPENSSL_VERSION = "1_1_1h";
+        my $ua = LWP::UserAgent->new;
+        my $response = $ua->get("https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.zip");
+        if (!$response->is_success) {
+            die "download failed: " . $response->status_line;
+        }
+
+        my $path = File::Spec->catfile($tmpdir, "openssl.tar.gz");
+        open my $fh, ">", $path or die "fail to open $path: $!";
+        binmode $fh;
+        print $fh $response->content;
+        close $fh;
+    };
+
+    group "extracting" => sub {
+        my $dir = pushd($tmpdir);
+        execute_or_die("7z x openssl.tar.gz -so | 7z x -si -ttar");
+    };
+
+    group "build and install OpenSSL" => sub {
+        # get the number of CPU cores to parallel make
+        my $jobs = ($ENV{NUMBER_OF_PROCESSORS} || 1) + 0;
+        if ($jobs <= 0) {
+            $jobs = 1;
+        }
+        execute_or_die('C:\strawberry\perl\bin\perl.exe Configure --prefix="' . $install_dir . '" mingw64');
+        execute_or_die('gmake');
+        execute_or_die('gmake install_sw');
+    };
+
     group "downloading perl $version from $url" => sub {
         my $ua = LWP::UserAgent->new;
         my $response = $ua->get($url);

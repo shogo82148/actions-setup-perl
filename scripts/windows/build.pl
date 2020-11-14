920700991b643bdf9074a29721b6b267ed1f2972
Ichinose Shogo <shogo82148@gmail.com>
2020-11-15 03:12:21 +0900

    import info subroutine

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -15,7 +15,7 @@ use File::pushd qw[pushd];
 use File::Spec;
 use File::Path qw/make_path remove_tree/;
 use Carp qw/croak/;
-use Actions::Core qw/group set_failed/;
+use Actions::Core qw/info group set_failed/;
 use File::Basename qw(dirname);
 
 my $version = $ENV{PERL_VERSION};

9e719f58c8cfde0329cb405104b238c8e7e483df
Ichinose Shogo <shogo82148@gmail.com>
2020-11-15 00:15:02 +0900

    cleanup the temprary diretory

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -18,6 +18,13 @@ use Carp qw/croak/;
 use Actions::Core qw/group set_failed/;
 use File::Basename qw(dirname);
 
+my $version = $ENV{PERL_VERSION};
+my $tmpdir = File::Spec->rel2abs($ENV{RUNNER_TEMP} || "tmp");
+make_path($tmpdir);
+my $install_dir = File::Spec->rel2abs(
+    File::Spec->catdir($ENV{RUNNER_TOOL_CACHE} || $tmpdir, "perl", $version, "x64"));
+my $perl = File::Spec->catfile($install_dir, 'bin', 'perl');
+
 sub perl_release {
     my $version = shift;
     my $releases = CPAN::Perl::Releases::MetaCPAN->new->get;
@@ -40,15 +47,13 @@ sub execute_or_die {
 sub run {
     local $ENV{PERL5LIB} = ""; # ignore libraries of the host perl
 
-    my $version = $ENV{PERL_VERSION};
     my $url = perl_release($version);
 
     $url =~ m/\/(perl-.*)$/;
     my $filename = $1;
-    my $tmpdir = File::Spec->rel2abs($ENV{RUNNER_TEMP} || "tmp");
-    make_path($tmpdir);
-    my $install_dir = File::Spec->rel2abs(
-        File::Spec->catdir($ENV{RUNNER_TOOL_CACHE} || $tmpdir, "perl", $version, "x64"));
+
+    # cleanup the temprary diretory
+    execute_or_die("rm -rf " . File::Spec->catfile($tmpdir, '*'));
 
     group "downloading perl $version from $url" => sub {
         my $ua = LWP::UserAgent->new;

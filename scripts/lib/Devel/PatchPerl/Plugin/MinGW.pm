5b659dae321e2b9420339e688325bb8872620b69
Ichinose Shogo <shogo82148@gmail.com>
2020-11-29 13:13:58 +0900

    fix _patch_system for v5.8.9

--- scripts/lib/Devel/PatchPerl/Plugin/MinGW.pm
+++ scripts/lib/Devel/PatchPerl/Plugin/MinGW.pm
@@ -4697,6 +4697,24 @@ sub _patch_system {
 PATCH
         return;
     }
+
+    if (_ge($version, "5.9.0")) {
+    _patch(<<'PATCH');
+--- embed.fnc
++++ embed.fnc
+@@ -171,7 +171,7 @@ Ap	|bool	|do_close	|GV* gv|bool not_implicit
+ p	|bool	|do_eof		|GV* gv
+ p	|bool	|do_exec	|char* cmd
+ #if defined(WIN32)
+-Ap	|int	|do_aspawn	|SV* really|SV** mark|SV** sp
++Ap	|int	|do_aspawn	|NULLOK SV* really|NN SV** mark|NN SV** sp
+ Ap	|int	|do_spawn	|char* cmd
+ Ap	|int	|do_spawn_nowait|char* cmd
+ #endif
+PATCH
+        return;
+    }
+
     _patch(<<'PATCH');
 --- embed.fnc
 +++ embed.fnc

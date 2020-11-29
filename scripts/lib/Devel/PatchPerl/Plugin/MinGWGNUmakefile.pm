50c24144c9496addc076cadc728bad2db492d81b
Ichinose Shogo <shogo82148@gmail.com>
2020-11-29 13:56:05 +0900

    fix v5.9.4

--- scripts/lib/Devel/PatchPerl/Plugin/MinGWGNUmakefile.pm
+++ scripts/lib/Devel/PatchPerl/Plugin/MinGWGNUmakefile.pm
@@ -8697,9 +8697,12 @@ PATCH
  $(MINIMOD) : $(HAVEMINIPERL) ..\minimod.pl
  	cd .. && miniperl minimod.pl > lib\ExtUtils\Miniperl.pm && cd win32
  
-@@ -745,6 +763,7 @@
+@@ -743,8 +761,9 @@
+ 	rem . > $@
+ 
  #most of deps of this target are in DYNALOADER and therefore omitted here
- Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM)
+-Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM)
++Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM) $(UNIDATAFILES)
  	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) $(EXTDIR)
 +	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) ext
  
@@ -8788,7 +8791,7 @@ PATCH
 @@ -762,8 +770,13 @@
  
  #most of deps of this target are in DYNALOADER and therefore omitted here
- Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM)
+ Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM) $(UNIDATAFILES)
 -	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) $(EXTDIR)
 -	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) ext
 +	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) $(EXTDIR) --dynamic
@@ -9048,7 +9051,7 @@ PATCH
 @@ -764,12 +819,14 @@
  
  #most of deps of this target are in DYNALOADER and therefore omitted here
- Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM)
+ Extensions : buildext.pl $(HAVEMINIPERL) $(PERLDEP) $(CONFIGPM) $(UNIDATAFILES)
 +	$(XCOPY) ..\\*.h $(COREDIR)\\*.*
  	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) $(EXTDIR) --dynamic
 -	$(MINIPERL) -I..\lib $(ICWD) buildext.pl "$(PLMAKE)" $(PERLDEP) ext --dynamic

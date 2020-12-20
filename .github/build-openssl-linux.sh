a08ddce88c4652cb6161e6690d63a7fc94a9c107
Ichinose Shogo <shogo82148@gmail.com>
2020-12-20 16:48:43 +0900

    bundle OpenSSL for better reproducibility (Linux) (#526)
    * bundle OpenSSL for better compatibility
    
    * fix comments
    
    * `git checkout HEAD^2` is no longer needed
    
    * add building OpenSSL step
    
    * configure OPENSSL_PREFIX
    
    * fix breaking GITHUB_ENV file
    
    * configure LD_LIBRARY_PATH
    
    * configure LD_LIBRARY_PATH
    
    * configure CFLAGS and LDFLAGS
    
    * fix OPENSSL_PREFIX
    
    * drop ubuntu-16.04

---
+++ .github/build-openssl-linux.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# bundle OpenSSL for better reproducibility.
+
+set -e
+
+OPENSSL_VERSION=1_1_1h
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
+PERL_DIR=perl
+if [[ "x$PERL_MULTI_THREAD" != "x" ]]; then
+    PERL_DIR="$PERL_DIR-thr"
+fi
+PREFIX=$RUNNER_TOOL_CACHE/$PERL_DIR/$PERL_VERSION/x64
+
+# detect the number of CPU Core
+JOBS=$(nproc)
+
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" linux-x86_64
+    make "-j$JOBS"
+    make install_sw
+)
+echo "::endgroup::"
+
+# configure for building Net::SSLeay
+cat <<__END__  >> "$GITHUB_ENV"
+OPENSSL_PREFIX=$PREFIX
+LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+__END__
+
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+sudo ldconfig

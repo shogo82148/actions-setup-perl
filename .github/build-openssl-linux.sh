7287906b567c6be237cd2fdc3851b0c3b32327c8
Ichinose Shogo <shogo82148@gmail.com>
2020-12-20 11:45:17 +0900

    bundle OpenSSL for better compatibility

---
+++ .github/build-openssl-linux.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# bundle OpenSSL for better compatibility.
+
+set -e
+
+OPENSSL_VERSION=1_1_1h
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
+PERL_DIR=perl
+if [[ "x$PERL_MULTI_THREAD" != "x" ]]; then
+    PERL_DIR="$PERL_DIR-thr"
+fi
+PREFIX=$RUNNER_TOOL_CACHE/$PERL_DIR/$PERL_VERSION/x64
+
+# detect the number of CPU Core
+JOBS=$(nproc)
+
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+rm -rf ./*
+
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" linux-x86_64
+    make "-j$JOBS"
+    make install_sw
+)
+echo "::endgroup::"

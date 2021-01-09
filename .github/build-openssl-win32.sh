22b303a3afce12a18b96839ba700418a1ed885f7
Ichinose Shogo <shogo82148@gmail.com>
2021-01-10 02:32:09 +0900

    add a script for building OpenSSL

---
+++ .github/build-openssl-win32.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# bundle OpenSSL for better reproducibility.
+
+set -eux
+
+OPENSSL_VERSION=1_1_1i
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
+PERL_DIR=perl
+if [[ "x$PERL_MULTI_THREAD" != "x" ]]; then
+    PERL_DIR="$PERL_DIR-thr"
+fi
+PREFIX=$(cygpath "$RUNNER_TOOL_CACHE\perl\${{ matrix.perl }}\x64")
+
+# detect the number of CPU Core
+JOBS=$(nproc)
+
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl --retry 3 -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" mingw64
+    make "-j$JOBS"
+    make install_sw install_ssldirs
+)
+echo "::endgroup::"

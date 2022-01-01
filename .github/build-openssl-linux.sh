da02e6e35fce61e8ff24d37e5796f5ea1a6595ee
Ichinose Shogo <shogo82148@gmail.com>
2022-01-01 21:00:51 +0900

    Bump openssl v1.1.1m (#972)
    * Revert "fix lib path for OpenSSL (#970)"
    
    This reverts commit 95d1789ef1c23b48ee85a2d64f6c5341b7321d3e.
    
    * Revert "bump openssl v3.0.1 (#968)"
    
    This reverts commit dc9c4158aab62ce2637f25d0a803e6bc5b0acecf.
    
    * bump OpenSSL v1.1.1m

--- .github/build-openssl-linux.sh
+++ .github/build-openssl-linux.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-OPENSSL_VERSION=3.0.1
+OPENSSL_VERSION=1_1_1m
 ROOT=$(cd "$(dirname "$0")" && pwd)
 : "${RUNNER_TEMP:=$ROOT/working}"
 : "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
@@ -24,7 +24,7 @@ echo "::group::download OpenSSL source"
 (
     set -eux
     cd "$RUNNER_TEMP"
-    curl --retry 3 -sSL "https://github.com/openssl/openssl/archive/openssl-$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+    curl --retry 3 -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
 )
 echo "::endgroup::"
 
@@ -39,7 +39,7 @@ echo "::endgroup::"
 echo "::group::build OpenSSL"
 (
     set -eux
-    cd "$RUNNER_TEMP/openssl-openssl-$OPENSSL_VERSION"
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
     ./Configure --prefix="$PREFIX" linux-x86_64
     make "-j$JOBS"
     make install_sw install_ssldirs
@@ -49,8 +49,8 @@ echo "::endgroup::"
 # configure for building Net::SSLeay
 cat <<__END__  >> "$GITHUB_ENV"
 OPENSSL_PREFIX=$PREFIX
-LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 __END__
 
-export LD_LIBRARY_PATH=$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 sudo ldconfig

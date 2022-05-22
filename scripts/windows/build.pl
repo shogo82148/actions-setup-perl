904d0e4f2555bc3645aa200c4942b5314e58dc4e
ICHINOSE Shogo <shogo82148@gmail.com>
2022-05-22 20:35:06 +0900

    bump JSON v4.06 (#1136)

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -170,7 +170,7 @@ sub run {
         cpan_install('https://cpan.metacpan.org/authors/id/J/JD/JDB/Win32-0.59.tar.gz', 'Win32', '5.6.0', '5.8.3');
 
         # JSON
-        cpan_install('https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.05.tar.gz', 'JSON', '5.5.3');
+        cpan_install('https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.06.tar.gz', 'JSON', '5.5.3');
 
         # Cpanel::JSON::XS
         cpan_install('https://cpan.metacpan.org/authors/id/R/RU/RURBAN/Cpanel-JSON-XS-4.28.tar.gz', 'Cpanel::JSON::XS', '5.6.2');

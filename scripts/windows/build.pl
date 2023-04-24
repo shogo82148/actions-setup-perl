9353f0df5dbe3379f6187d5cb7273c96738f1650
ICHINOSE Shogo <shogo82148@gmail.com>
2023-04-24 09:25:40 +0900

    bump Mozilla::CA v20221114

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -227,7 +227,7 @@ sub run {
         cpan_install('https://cpan.metacpan.org/authors/id/C/CH/CHRISN/Net-SSLeay-1.92.tar.gz', 'Net-SSLeay', 'Net::SSLeay', '5.8.1');
 
         # Mozilla::CA
-        cpan_install('https://cpan.metacpan.org/authors/id/A/AB/ABH/Mozilla-CA-20211001.tar.gz', 'Mozilla-CA', 'Mozilla::CA', '5.6.0');
+        cpan_install('https://cpan.metacpan.org/authors/id/H/HA/HAARG/Mozilla-CA-20221114.tar.gz', 'Mozilla-CA', 'Mozilla::CA', '5.6.0');
 
 
         # IO::Socket::SSL

1ae334f633e091fc1826c924077e999411ff24ce
ICHINOSE Shogo <shogo82148@gmail.com>
2023-08-04 11:57:49 +0900

    auto update OpenSSL (#1590)
    * auto update OpenSSL
    
    * configure GH_TOKEN environment

---
+++ .github/update-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+cd "$CURRENT"
+
+set -eu
+OPENSSL_VERSION=$(gh api --jq 'last.ref | sub("refs/tags/openssl-"; "")' /repos/openssl/openssl/git/matching-refs/tags/openssl-3.)
+export OPENSSL_VERSION
+perl -i -pe 's/^OPENSSL_VERSION=.*$/OPENSSL_VERSION=$ENV{OPENSSL_VERSION}/' build-openssl-darwin.sh
+perl -i -pe 's/^OPENSSL_VERSION=.*$/OPENSSL_VERSION=$ENV{OPENSSL_VERSION}/' build-openssl-linux.sh
+perl -i -pe 's/^OPENSSL_VERSION=.*$/OPENSSL_VERSION=$ENV{OPENSSL_VERSION}/' build-openssl-win32.sh

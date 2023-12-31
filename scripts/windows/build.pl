5b1d0d4d2aeeb8435e613103de5f72f60d40649a
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-31 19:22:15 +0900

    bump IO::Socket::SSL v2.084

--- scripts/windows/build.pl
+++ scripts/windows/build.pl
@@ -248,7 +248,7 @@ sub run {
         # https://github.com/shogo82148/actions-setup-perl/pull/480#issuecomment-735391122
         local $ENV{NO_NETWORK_TESTING} = 1;
         local $ENV{PERL_MM_USE_DEFAULT} = 1;
-        cpan_install('https://cpan.metacpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-2.083.tar.gz', 'IO-Socket-SSL', 'IO::Socket::SSL', '5.8.1');
+        cpan_install('https://cpan.metacpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-2.084.tar.gz', 'IO-Socket-SSL', 'IO::Socket::SSL', '5.8.1');
 
         # Test::Harness
         cpan_install('https://cpan.metacpan.org/authors/id/L/LE/LEONT/Test-Harness-3.44.tar.gz', 'Test-Harness', 'Test::Harness', '5.6.0', '5.8.3');

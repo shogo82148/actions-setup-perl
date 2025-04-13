3f562ba68a4f47809be7efed747266621453893e
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-13 22:45:18 +0900

    verify build provenance

--- src/setup-perl.ts
+++ src/setup-perl.ts
@@ -12,6 +12,7 @@ async function run() {
   try {
     const platform = process.platform;
     let dist = core.getInput("distribution");
+    const githubToken = core.getInput("github-token");
     const multiThread = core.getInput("multi-thread");
     const version = await resolveVersionInput();
 
@@ -39,7 +40,7 @@ async function run() {
           result = await strawberry.getPerl(version);
           break;
         case "default":
-          result = await installer.getPerl(version, thread);
+          result = await installer.getPerl(version, thread, githubToken);
           break;
         default:
           throw new Error(`unknown distribution: ${dist}`);

df60a93d5526a4f49986617a023a476c5305c4ca
ICHINOSE Shogo <shogo82148@gmail.com>
2025-04-13 22:52:19 +0900

    add GITHUB_TOKEN to workflow and tests for authentication

--- __test__/installer.test.ts
+++ __test__/installer.test.ts
@@ -3,6 +3,7 @@ import path = require("path");
 import os = require("os");
 import fs = require("fs");
 
+const githubToken = process.env["GITHUB_TOKEN"] || "";
 const toolDir = path.join(__dirname, "runner", "tools");
 const tempDir = path.join(__dirname, "runner", "temp");
 // const dataDir = path.join(__dirname, 'data');
@@ -30,7 +31,7 @@ describe("installer tests", () => {
   }, 100000);
 
   it("Acquires version of Perl if no matching version is installed", async () => {
-    await installer.getPerl("5.26.x", false);
+    await installer.getPerl("5.26.x", false, githubToken);
     const perlDir = path.join(toolDir, "perl", "5.26.3", os.arch());
 
     expect(fs.existsSync(`${perlDir}.complete`)).toBe(true);
